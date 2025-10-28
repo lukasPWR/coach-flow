ee21cc4ae84909c1ed62241c7ed39a782e14e772
Lukasz Nowicki <lukasz.nowicki@euvic.pl>
2025-10-28 19:36:28 +0100

    feat:adding swagger, helmer, changing eslint

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,35 +1,48 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import typescriptEslintEslintPlugin from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["**/.eslintrc.js"],
+}, ...compat.extends("plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"), {
+    plugins: {
+        "@typescript-eslint": typescriptEslintEslintPlugin,
+    },
+
     languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+        globals: {
+            ...globals.node,
+            ...globals.jest,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "module",
+
+        parserOptions: {
+            project: "tsconfig.json",
+            tsconfigRootDir: __dirname,
+        },
     },
-  },
-  {
+
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "no-console": "warn",
+        "no-inline-comments": "error",
     },
-  },
-);
+}];
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,35 @@
 import { NestFactory } from "@nestjs/core";
 import { initializeTransactionalContext } from "typeorm-transactional";
 import { AppModule } from "./app.module";
+import helmet from "helmet";
+import { SwaggerModule, DocumentBuilder, SwaggerCustomOptions } from "@nestjs/swagger";
 
 // Initialize transactional context for TypeORM
 initializeTransactionalContext();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix("api");
+
+  app.use(helmet());
+  app.enableCors();
+
+  if (process.env.OPEN_API_AVAIBLE) {
+    const config = new DocumentBuilder().setTitle("CoachFlow API Docs").setVersion("1.0").addBearerAuth().build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    const customOptions: SwaggerCustomOptions = {
+      swaggerOptions: {
+        persistAuthorization: true,
+        tagsSorter: "alpha",
+        operationsSorter: "alpha",
+      },
+      customSiteTitle: "CoachFlow API Docs",
+    };
+
+    SwaggerModule.setup("api", app, documentFactory, customOptions);
+  }
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
